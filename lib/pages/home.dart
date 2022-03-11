44ab2d795b1b523019b4d9dd4d1c63c7d91a736e
spacemeowx2 <spacemeowx2@gmail.com>
2022-03-12 05:39:08 +0800

    refactor: move home page

---
+++ lib/pages/home.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:rdp_flutter/pages/server_selector.dart';
+import 'package:rdp_flutter/provider/rdp_model.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final setSelected = Provider.of<SetSelected>(context);
+
+    return Consumer<RDPModel>(builder: (context, rdp, child) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.logout),
+              onPressed: () {
+                setSelected.set(null);
+              },
+            ),
+          ],
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('Content: ' + rdp.server.inlineDescription()),
+            ],
+          ),
+        ),
+      );
+    });
+  }
+}
