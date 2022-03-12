539bd49e2f8befade15a6827c1e798c958a04de6
spacemeowx2 <spacemeowx2@gmail.com>
2022-03-12 15:22:12 +0800

    build: add websocket

--- lib/provider/rdp_model.dart
+++ lib/provider/rdp_model.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/foundation.dart';
 import 'package:rdp_flutter/model.dart';
+import 'package:web_socket_channel/io.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
 
 class RDPModel extends ChangeNotifier {
   final ServerItem server;
+  late final channel = IOWebSocketChannel.connect(server.url);
 
   RDPModel({required this.server}) : super();
 }
